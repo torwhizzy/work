3d38dd782fa8655d7d685d05235d08d9657e679d
torwhizzy <39039062+torwhizzy@users.noreply.github.com>
2021-05-08 01:27:11 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// navbar for toggle menu
+var navLink = document.getElementById('navlink');
+function showMenu(){
+    navLink.style.right = "0";
+}
+function hideMenu(){
+    navLink.style.right = "-200px";
+}
