cb5bca01479723e0ce6c73d0ed86b605497dccee
torwhizzy <39039062+torwhizzy@users.noreply.github.com>
2021-07-16 22:02:48 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-// navbar for toggle menu
-var navLink = document.getElementById('navlink');
-function showMenu(){
-    navLink.style.right = "0";
-}
-function hideMenu(){
-    navLink.style.right = "-200px";
-}
